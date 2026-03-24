e5afdf4980652424acf10716e31694fa34a10e11
Alessandro Tiberia <tiberia7030@gmail.com>
2026-03-24 15:24:31 +0100

    aggiunto multiplo di 3 e multiplo di 5

--- script.js
+++ script.js
@@ -8,11 +8,28 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 //Come faccio a sapere se un numero è divisibile per?
 //Abbiamo visto qualcosa di particolare che possiamo usare?
 
+const numero = (parseInt(prompt("inserisci numero")));
+
 //stampiamo numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
+
     //se sono multipli di 3 e 5
     if ((i % 3 === 0) && (i % 5 === 0)) {
-        console.log("è multiplo di 3 e 5, quindi FIZZBUZZ");
 
+        console.log("è multiplo di 3 e 5, quindi FIZZBUZZ");
+        
+        //multipli di 3
+    } else if (i % 3 === 0) { 
+
+        console.log("multipli di 3, quindi FIZZ");
+        
+        // se sono multipli di 5
+    } else if (i % 5 === 0 ){
+        
+        console.log("multipli di 5, quindi BUZZ");
+        
+    } else {
+        console.log(i);
+        
     }
 }
